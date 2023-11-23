17b2d8799974e46275060c5d804655e309ef9d46
Gavriel770U <kraserultra@gmail.com>
2023-11-23 15:34:37 +0200

    Update phase switching

--- src/creepy.py
+++ src/creepy.py
@@ -1,12 +1,14 @@
 import pyautogui
 import time
 
-MAX_PHASE: int = 1
+MAX_PHASE: int = 2
 
 class CreePy:
     def __init__(self) -> None:
         self._phase: int = 1
         self._phase_duration: int = 10 # seconds
+        self._phase_switch_sleep: int = 5 # seconds
+        self.__PHASES: dict = {1 : self._phase_one, 2 : self._phase_one}
     
     def _phase_one(self) -> None:
         end_time = time.time() + self._phase_duration
@@ -15,4 +17,6 @@ class CreePy:
     
     def next_phase(self) -> None:
         if self._phase < MAX_PHASE:
-            self._phase += 1  
+            self._phase += 1
+        time.sleep(self._phase_switch_sleep)
+        self.__PHASES[self._phase]()
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from creepy import *
 def main() -> None:
     creepy = CreePy()
     creepy._phase_one()
+    creepy.next_phase()
 
 if __name__ == '__main__':
     main()
